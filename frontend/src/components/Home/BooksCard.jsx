58f61b75a19d9c1e5011b0562579e22fdcbf1c8a
Waseq225 <mohammadwaseq225@gmail.com>
2024-06-25 19:45:59 +1000

    card component

--- frontend/src/components/Home/BooksCard.jsx
+++ frontend/src/components/Home/BooksCard.jsx
@@ -1,6 +1,12 @@
+import { BookSingleCard } from './BookSingleCard';
 
-export const BooksCard = () => {
+export const BooksCard = ({ books }) => {
   return (
-    <div>BooksCar</div>
-  )
-}
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {books.map((item) => (
+        <BookSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+
